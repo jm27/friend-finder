1f4f81401aa3ebb6e86ade55734fc86b6eba033e
Jesus Esquer <jme2791@gmail.com>
2019-11-06 14:43:56 -0700

    Added html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,24 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+
+let path = require("path");
+
+
+module.exports = function(app) {
 //A default, catch-all route that leads to `home.html` 
 //Which displays the home page.
 
-
-
-
-
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 //A GET Route to `/survey` which should display the survey page.
 
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
 
-
+};
