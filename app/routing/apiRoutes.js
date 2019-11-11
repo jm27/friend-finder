20e1c61d8b8f33735e6d77540d74a1d4480555f1
Jesus Esquer <jme2791@gmail.com>
2019-11-10 19:16:31 -0700

    added style to modal image height and widht

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,6 @@
 let friendsData = require("../data/friends");;
 let userData = require("../data/user");
 
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -28,24 +27,20 @@ module.exports = function (app) {
     let userScores = req.body.scores;
     let friend = 0;
 
-
     //Post Method Variables
     //Array of numbers
     let Array1 = friendsData[0].scores;
     let Array2 = friendsData[1].scores;
     let Array3 = friendsData[2].scores;
     let ArrayFour = userScores;
-    console.log(ArrayFour);
-    console.log(Array1);
 
     // Empty arrays to pass in Differences
     let totalOne = [];
     let totalTwo = [];
     let totalThree = [];
+
     //This loop is used to subtract each array elements,
     //Comparing User Data with Friends
-    console.log(totalOne);
-
     for (var i = 0; i < ArrayFour.length; i++) {
       //Use the absolute value of the differences,
       //Of each array element
@@ -54,12 +49,6 @@ module.exports = function (app) {
       totalTwo.push(Math.abs(ArrayFour[i] - Array2[i]));
       totalThree.push(Math.abs(ArrayFour[i]- Array3[i]));
     };
-    console.log(ArrayFour);
-    console.log(Array1);
-
-    console.log(totalOne);
-    console.log(totalTwo);
-    console.log(totalThree);
     //Function to sum
     function sumArray(total, num) {
       return total + num;
@@ -69,10 +58,7 @@ module.exports = function (app) {
     totalOne = parseInt(totalOne);
     totalTwo = totalTwo.reduce(sumArray);
     totalThree = totalThree.reduce(sumArray);
-    console.log(totalOne);
-    console.log(totalTwo);
-    console.log(totalThree);
-
+    
     //If statements to compare Total differences 
     //And get the least amount of diffence. 
     if (totalOne < totalTwo && totalOne < totalThree) {
